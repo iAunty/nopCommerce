98a26bddfcee2c0fd0fa0fb2ae7d4b81b95728b2
為什麼 <andy840119@gmail.com>
2018-05-23 11:13:23 +0800

    add class

---
+++ src/Libraries/Nop.Core/BaseStringIdEntity.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Azure.Mobile.Server.Tables;
+
+namespace Nop.Core
+{
+    /// <summary>
+    /// Base Guid entity 
+    /// is for object that id is GUID
+    /// </summary>
+    public abstract class BaseStringIdEntity : IBaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the entity identifier
+        /// </summary>
+        [Key]
+        [TableColumn(TableColumnType.Id)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Is Equal
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as BaseStringIdEntity);
+        }
+
+        /// <summary>
+        /// Is Transient
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        private static bool IsTransient(BaseStringIdEntity obj)
+        {
+            return obj != null && Equals(obj.Id, default(string));
+        }
+
+        /// <summary>
+        /// Get type 
+        /// </summary>
+        /// <returns></returns>
+        private Type GetUnproxiedType()
+        {
+            return GetType();
+        }
+
+        /// <summary>
+        /// is equal
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public virtual bool Equals(BaseStringIdEntity other)
+        {
+            if (other == null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (!IsTransient(this) &&
+                !IsTransient(other) &&
+                Equals(Id, other.Id))
+            {
+                var otherType = other.GetUnproxiedType();
+                var thisType = GetUnproxiedType();
+                return thisType.IsAssignableFrom(otherType) ||
+                       otherType.IsAssignableFrom(thisType);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Get hesh code
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            if (Equals(Id, default(int)))
+                return base.GetHashCode();
+            return Id.GetHashCode();
+        }
+
+        /// <summary>
+        /// operator
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator ==(BaseStringIdEntity x, BaseStringIdEntity y)
+        {
+            return Equals(x, y);
+        }
+
+        /// <summary>
+        /// operator
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator !=(BaseStringIdEntity x, BaseStringIdEntity y)
+        {
+            return !(x == y);
+        }
+    }
+}
---
+++ src/Libraries/Nop.Core/BaseSyncableEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Azure.Mobile.Server.Tables;
+
+namespace Nop.Core
+{
+    /// <summary>
+    /// this class is used for syncable object
+    /// </summary>
+    public class BaseSyncableEntity : BaseStringIdEntity , ITableData
+    {
+        /// <summary>
+        /// Create At
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //[Index(IsClustered = true)]
+        [TableColumn(TableColumnType.CreatedAt)]
+        public DateTimeOffset? CreatedAt { get; set; }
+
+        /// <summary>
+        /// Update At
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [TableColumn(TableColumnType.UpdatedAt)]
+        public DateTimeOffset? UpdatedAt { get; set; }
+
+        /// <summary>
+        /// Version
+        /// </summary>
+        [TableColumn(TableColumnType.Version)]
+        [Timestamp]
+        public byte[] Version { get; set; }
+
+        /// <summary>
+        /// Deleted
+        /// </summary>
+        [TableColumn(TableColumnType.Deleted)]
+        public bool Deleted { get; set; }
+    }
+}
